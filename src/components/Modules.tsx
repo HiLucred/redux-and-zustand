4cab619b8e7ac0e3ccb476ce4df57ea3a7a7540e
Luc <lucred@protonmail.com>
2023-08-07 14:14:27 -0300

    Initial commit

---
+++ src/components/Modules.tsx
@@ -0,0 +1,56 @@
+import { ChevronDown } from "lucide-react";
+import { Lesson } from "./Lesson";
+import * as Collapsible from '@radix-ui/react-collapsible';
+import { useStore } from "../zustand-store";
+
+interface ModuleProps {
+  moduleIndex: number
+  title: string
+  amountOfLessons: number
+}
+
+export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
+  const { lessons, currentModuleIndex, currentLessonIndex, play } = useStore((state) => {
+    return {
+      lessons: state.course?.modules[moduleIndex].lessons,
+      currentModuleIndex: state.currentModuleIndex,
+      currentLessonIndex: state.currentLessonIndex,
+      play: state.play,
+    }
+  })
+  
+  return (
+    <Collapsible.Root className="group" defaultOpen={moduleIndex === 0}>
+      <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
+        <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-sm">{moduleIndex + 1}</div>
+
+        <div className="flex flex-col gap-1 text-left">
+          <strong className="text-sm">{title}</strong>
+          <span className="text-xs text-zinc-400">{amountOfLessons + " aulas"}</span>
+        </div>
+
+        <ChevronDown className="w-4 h-4 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
+      </Collapsible.Trigger>
+
+      <Collapsible.Content>
+
+        <nav className="relative flex flex-col gap-4 p-6">
+          {lessons && lessons.map((lesson, lessonIndex) => {
+            const isCurrent = moduleIndex === currentModuleIndex &&
+              lessonIndex === currentLessonIndex
+
+            return (
+              <Lesson 
+                key={lesson.id}
+                title={lesson.title}
+                duration={lesson.duration}
+                isCurrent={isCurrent}
+                onPlay={() => play([moduleIndex, lessonIndex])}
+              />
+            )
+          })}
+        </nav>
+      </Collapsible.Content>
+    </Collapsible.Root>
+  )
+}
---
+++ src/pages/Player.tsx
@@ -0,0 +1,61 @@
+import { MessageCircle } from "lucide-react"
+import { Header } from "../components/Header"
+import { Video } from "../components/Video"
+import { Module } from "../components/Modules"
+import { useEffect } from "react"
+import { useCurrentLesson, useStore } from "../zustand-store"
+
+export function Player() {
+  const { currentLesson } = useCurrentLesson()
+
+  const { course, load } = useStore((state) => {
+    return {
+      course: state.course,
+      load: state.load
+    }
+  })
+  
+  useEffect(() => {
+    load()
+  }, [])
+
+  useEffect(() => {
+    if (currentLesson) {
+      document.title = `Assistindo: ${currentLesson.title}`
+    }
+  }, [currentLesson])
+
+  return (
+    <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
+      <div className="flex w-[1100px] flex-col gap-6">
+        <div className="flex items-center justify-between">
+          <Header />
+
+          <button className="flex items-center gap-2 rounded bg-violet-500 px-3 py-2 text-sm font-medium text-white hover:bg-violet-600">
+            <MessageCircle className="w-4 h-4" />
+            Deixar feedback
+          </button>
+        </div>
+
+        <main className="relative flex overflow-hidden pr-80 rounded-lg border border-zinc-800 bg-zing-900 shadow">
+          <div className="flex-1">
+            <Video />
+          </div>
+
+          <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-700">
+            {course?.modules && course.modules.map((module, index) => {
+              return (
+                <Module 
+                  key={module.id}
+                  moduleIndex={index} 
+                  title={module.title} 
+                  amountOfLessons={module.lessons.length}
+                />
+              )
+            })}
+          </aside>
+        </main>
+      </div>
+    </div>
+  )
+}
